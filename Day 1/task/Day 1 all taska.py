20a0a27441c46ac23e230908b29132a3c1f8c194
ihabiba <onlyhabiba1.com>
2025-08-31 11:11:04 +0300

    commit

---
+++ Day 1/task/Day 1 all taska.py
@@ -0,0 +1,65 @@
+# =========================
+# Day 1 - Task 1: Printing
+# =========================
+# Write your code below this line 👇
+print("Hello world!")
+
+
+# =========================
+# Day 1 - Task 2: String Manipulation
+# =========================
+print("Hello world!")
+
+# 1. New Line Escape Sequence
+print("Hello")
+print("Word")
+
+# is the same as:
+print("Hello\nWorld")
+
+# 2. String Concatenation
+print("My name is" + " " + "Angela")
+# Output: My name is Angela
+
+
+# =========================
+# Day 1 - Task 3: Inputs
+# =========================
+# This line of code will take an input using the input() function
+print("Hello " + input("What is your name?") + "!")
+
+
+# =========================
+# Day 1 - Task 4: Variables
+# =========================
+input("What is your name?")
+
+name = "Jack"
+print(name)
+
+name = "Habiba"
+print(name)
+
+# Example with length of input
+# print(len(input("What is your name?")))
+
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+
+# =========================
+# Day 1 - Task 5: Variable Naming
+# =========================
+name = "Angela"
+length = len(name)
+print(length)
+
+
+# =========================
+# Day 1 - Task 6: Band Name Generator Project
+# =========================
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
